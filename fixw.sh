dc9911bbaa783d267bc6d83195f7a085008f9f0b
Khalil <a.khalilelhassar@gmail.com>
2023-04-10 15:49:17 +0000

    initial commit

---
+++ fixw.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pwd=$PWD
+
+# Update package index
+sudo apt update
+
+# Install firmware
+sudo apt install -y firmware-iwlwifi
+
+# Load driver
+sudo modprobe -vv iwlwifi
+
+# Add fixw.sh to crontab
+if ! crontab -l | grep -q "@reboot $(pwd)/fixw.sh"; then
+  (crontab -l; echo "@reboot $(pwd)/fixw.sh") | crontab - && echo "[+] Added fixw.sh to crontab"
+else
+  echo "[-] fixw.sh is already in the crontab file"
+fi
